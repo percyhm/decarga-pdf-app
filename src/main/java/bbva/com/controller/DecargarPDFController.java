d6a9471f34dcf1a413557d520da5505736445d0d
hgutierrez.analista <hgutierrez.analista@gmail.com>
2024-10-14 15:08:33 -0500

    feat: fileupload

--- src/main/java/bbva/com/controller/DecargarPDFController.java
+++ src/main/java/bbva/com/controller/DecargarPDFController.java
@@ -1,5 +1,7 @@
 package bbva.com.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,20 +10,34 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("descarga")
 public class DecargarPDFController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DecargarPDFController.class);
+
     @GetMapping(value ="/mostrarPdf")
     public String descargar(){
+        LOGGER.info("descargar::Inicio---");
         RestTemplate restTemplate = new RestTemplate();
 //        Map<String, String> vars = new HashMap<>();
 //        vars.put("hotel", "42");
 //        vars.put("booking", "21");
 //        String result = restTemplate.getForObject("http://example.com/hotels/{hotel}/bookings/{booking}", String.class, vars);
-        //String result = restTemplate.getForObject("https://documentos-royal.rimac.com/vida/vividaaddons/planesvidainversion/poliza/2024/08/21/83388d79-31c5-41b5-9541-f273889bfc24.pdf", String.class);
+        byte[] response = null;
+        try{
+            response = restTemplate.getForObject("https://documentos-royal.rimac.com/vida/vividaaddons/planesvidainversion/poliza/2024/08/21/83388d79-31c5-41b5-9541-f273889bfc24.pdf", byte[].class);
+        }catch (Exception e){
+            LOGGER.error("Error", e);
+        }
+
+        if(response!=null){
+            LOGGER.info("byte length :: {}" , response.length);
+        }
         String result = "response";
         System.out.println(result);
+        LOGGER.info("descargar::Fin---");
         return result;
     }
 }
